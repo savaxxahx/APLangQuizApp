c4f85b59aaad59e94ef760cd915bb0884871ab4d
savaxxahx <savannah@katherynsavannah.com>
2017-09-06 10:53:15 -0700

    final touches

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 $(document).ready(function(){
-	$(".run").hide(function(){
-		});
+	$("#button1").show();
 		$("#button1").click(function(){
-			
-		
-		$(".run").slideToggle();
-		
+		$("#p2").slideToggle();
+		$("#button1").hide();
+		$("#button2").show();
 	});
+	
+	$("#button2").click(function(){
+		$("#p2").hide();
+		$("#p4").slideToggle();
+		$("#button2").hide();
+		$("#button3").show();
+		});
+		
+		$("#button3").click(function(){
+		$("#p4").hide();
+		$("#p6").slideToggle();
+		$("#button3").hide()
+		$("#button4").show();;
+		});
+		
+		$("#button4").click(function(){
+		$("#p6").hide();
+		$("#p8").slideToggle();
+		$("#button4").hide();
+		$("#button5").show();
+		});
+		
+		$("#button5").click(function(){
+		$("#p8").hide();
+		$("#p10").slideToggle();
+		$("#button5").hide();
+		$("#button6").show();
+		});
+		
+		$("#button6").click(function(){
+		$("#p10").hide();
+		$("#p12").slideToggle();
+		$("#button6").hide();
+		});
 });
