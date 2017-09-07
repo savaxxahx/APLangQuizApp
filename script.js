c776783bea72eefc627dfbdea18e76893c0d74c2
savaxxahx <savannah@katherynsavannah.com>
2017-09-07 09:08:42 -0700

    added hide answers button

--- script.js
+++ script.js
@@ -1,49 +1,54 @@
 $(document).ready(function(){
 	$("#button1").show();
 	$("#button7").show();
+	$("#button8").show();
+	
 		$("#button1").click(function(){
-		$("#p2").slideToggle();
+		$("#p2").slideDown();
 		$("#button1").hide();
 		$("#button2").show();
 	});
 	
 	$("#button2").click(function(){
 		$("#p2").hide();
-		$("#p4").slideToggle();
+		$("#p4").slideDown();
 		$("#button2").hide();
 		$("#button3").show();
 		});
 		
 		$("#button3").click(function(){
 		$("#p4").hide();
-		$("#p6").slideToggle();
+		$("#p6").slideDown();
 		$("#button3").hide()
 		$("#button4").show();;
 		});
 		
 		$("#button4").click(function(){
 		$("#p6").hide();
-		$("#p8").slideToggle();
+		$("#p8").slideDown();
 		$("#button4").hide();
 		$("#button5").show();
 		});
 		
 		$("#button5").click(function(){
 		$("#p8").hide();
-		$("#p10").slideToggle();
+		$("#p10").slideDown();
 		$("#button5").hide();
 		$("#button6").show();
 		});
 		
 		$("#button6").click(function(){
-		$("#p10").slideToggle();
+		$("#p10").slideDown();
 		$("#p12").show();
 		$("#button6").hide();
 		});
 		
 		$("#button7").click(function(){
 		$(".run").show();
+		$("#button1").show();	
+		});
 		
-		
+		$("#button8").click(function(){
+			$(".run").hide();
 		});
 });
